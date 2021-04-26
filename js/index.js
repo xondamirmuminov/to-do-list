b19ab10bed4b8524d1df3bf0453b045ce8186dc8
Xondamir Mo'minov <xondamir.muminov2008@gmail.com>
2021-04-26 10:37:43 +0500

    add new changes

--- js/index.js
+++ js/index.js
@@ -12,22 +12,37 @@ var noCheck = document.querySelector('#not-text');
 var checkText = document.querySelector('#check-text');
 var list = document.querySelector('.form__list');
 var listItem = document.querySelector('.form__list-item');
-
+var audio = document.querySelector('.form__audio');
+var listInp = document.querySelector('.form__list-inp');
+var time = document.querySelector('.form__time');
+var btnNone = true;
+setInterval(function(){
+    time.innerHTML =  new Date().toLocaleTimeString('ru');;
+}, 000);
 function task(e) {
     e.preventDefault();
     // var addList = document.createElement("li");
     // var nodeList = document.createTextNode(`
     //     <div class='form__list-item'>
     //     ${formInp.value}
-    //         <button class="btn-not" onclick="check()">
+    //         <button class="btn-none" onclick="check()">
     //             <a id="not-text" href="#">❌</a> 
     //             <a id="check-text" href="#">✅</a>
     //         </button>
     //     </div>
     // `);
+    setInterval(function (){
+        var date = new Date().toLocaleTimeString('ru', { hour: '2-digit', minute: '2-digit' });
+        console.log(date);
+        if(btnNone == true && date == listInp.value ) {
+            audio.play();
+        }else {
+              audio.pause();}
+    }, 3000);
+
     var template = `<li class="form__list-item">
         ${formInp.value}
-        <button class="btn-not" onclick="check(this)">
+        <button class="btn-none" onclick="check(this)">
             <a id="not-text" href="#">❌</a> 
          </button>
    </li>`;
@@ -35,7 +50,19 @@ function task(e) {
     list.innerHTML += template;
     // list.appendChild(nodeList);
 };
-
 function check(el){
-    el.parentElement.classList.add('form__list-item--deleted');
+    audio.pause();
+    btnNone = false;
+    el.parentElement.classList.add('form__list-item--deleted')
+
 };
+
+
+
+// var n = 9.4468948;
+// console.log(n.toFixed(2));
+// console.log(n.toPrecision(3));
+// console.log(n.toFixed(2));
+
+// var day = new Date();
+// console.log(day.getMonth());
